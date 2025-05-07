dc7b49989b455ba48a22cdb9c90d79c6084158e5
Ahmed Fawzy <138530280+AhmedFawzy10@users.noreply.github.com>
2025-05-08 02:30:09 +0300

    [239] [Time Beats: 7.85%] [Memory Beats: 5.52%] - LeetPush

--- test/Sliding-Window-Maximum.cpp
+++ test/Sliding-Window-Maximum.cpp
@@ -1,47 +1,17 @@
 class Solution {
 public:
-    struct MonotonicQueue{
-        queue<int> q;
-        deque<int> pq;
-        void push(int x)
-        {
-            while(!pq.empty() && x > pq.back())
-            {
-                pq.pop_back();
-            }
-            pq.push_back(x);
-            q.push(x);
-        }
-        void pop()
-        {
-            if(pq.front() == q.front()) pq.pop_front();
-            q.pop();
-        }
-        int max()
-        {
-            return pq.front();
-        }
-        bool empty()
-        {
-            return q.empty();
-        }
-    };
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n = nums.size();
         vector<int> ans;
-        MonotonicQueue mq;
-        for(int i = 0; i < n; i++)
+        multiset<int> ms;
+        for(int i = 0; i < k; ++i) ms.insert(nums[i]);
+        ans.push_back(*ms.rbegin());
+        for(int i = k; i < n; ++i)
         {
-            if(i < k)
-                mq.push(nums[i]);
-            else
-            {
-                ans.push_back(mq.max());
-                mq.push(nums[i]);
-                mq.pop();
-            }
+            ms.erase(ms.find(nums[i-k]));
+            ms.insert(nums[i]);
+            ans.push_back(*ms.rbegin());
         }
-        if(!mq.empty())ans.push_back(mq.max());
         return ans;
     }
 };
