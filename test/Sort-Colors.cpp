291ca3e3a890510f063016083631a0a6fcac8a6d
Ahmed Fawzy <138530280+AhmedFawzy10@users.noreply.github.com>
2025-05-17 12:31:15 +0300

    [75] [Time Beats: 100.00%] [Memory Beats: 97.40%] - LeetPush

---
+++ test/Sort-Colors.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int freq[3];
+        for(int &i : nums)freq[i]++;
+        int j = 0;
+        for(int i = 0; i < 3; i++)
+            while(freq[i]--)nums[j++] = i;
+    }
+};
